d16f162a739c62e3dcdd1d783d2d1a855410c83a
Despo <tornike.despotashvili@gmail.com>
2024-10-25 15:38:03 +0400

    feat/ add navigation bar to details screen

--- assignment-18-movie-manager/FavoriteScreen/view/DetailsVC.swift
+++ assignment-18-movie-manager/FavoriteScreen/view/DetailsVC.swift
@@ -7,23 +7,43 @@
 
 import UIKit
 
-class DetailsVC: UIViewController {
+final class DetailsVC: UIViewController {
+    private let navigationView = UIStackView()
+    private let customBackButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupUI() {
+        setupCustomNavigation()
+        setupCustomBackButton()
     }
-    */
+    
+    private func setupCustomNavigation() {
+        view.addSubview(navigationView)
+        
+        navigationView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            navigationView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        ])
+    }
+    
+    private func setupCustomBackButton() {
+        navigationView.addArrangedSubview(customBackButton)
 
+        customBackButton.setImage(UIImage(named: "BackIcon"), for: .normal)
+        customBackButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            customBackButton.leadingAnchor.constraint(equalTo: navigationView.leadingAnchor, constant: 24)
+        ])
+        
+        customBackButton.addAction(UIAction(handler: { [weak self] action in
+            self?.navigationController?.popViewController(animated: true)
+        }), for: .touchUpInside)
+    }
 }
--- assignment-18-movie-manager/MainScreen/view/MainScreenVC.swift
+++ assignment-18-movie-manager/MainScreen/view/MainScreenVC.swift
@@ -18,6 +18,7 @@ class MainScreenVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
 
         setupTestButton()
         view.backgroundColor = .purplePrimary
